10669037885750f3c58885d1d9f08b88d3774d06
Ivan Montiel <idmontie@gmail.com>
2016-12-08 10:32:46 -0700

    Add verbose flag, don't print stdout
    Close #4 and #5

--- scripts/docs.js
+++ scripts/docs.js
@@ -7,8 +7,9 @@ var fs = require('fs');
 var program = require('commander');
 
 program.arguments('<files>')
-  .option('-i, --ignore <ignore>', 'Files to ignore')
+  .option('-i, --ignore <ignore...>', 'Files to ignore')
   .option('-c, --config <config>', 'Configuration file for jsdoc')
+  .option('-v, --verbose', 'Print out all files we look at and their result')
   .action(function(files) {
     var options = {};
 
@@ -18,20 +19,32 @@ program.arguments('<files>')
 
     var globFiles = glob.sync(files, options);
 
+    if (program.verbose) {
+      console.log('> Checking the following files:');
+      globFiles.forEach(f => console.log('>>> ' + f));
+      console.log('');
+
+      console.log('> Parsing:');
+    }
+
     globFiles.forEach(f => {
       // Don't write the file if it would be empty
       exec(
         './node_modules/jsdoc-to-markdown/bin.js ' +
         f + (program.config ? (' -c ' + program.config + ' ') : ''),
         function (error, stdout, stderr) {
-          if (error || stdout) {
-            console.error(error || stdout);
+          if (error || stderr) {
+            console.error(error || stderr);
           }
 
           if (stdout.trim() !== '') {
             fs.writeFile(f.replace('.js', '.md'), stdout, function callback() {
+              program.verbose ? console.log('>> ' + f) : null;
               console.log('>>> Generated docs for ' + f);
             });
+          } else if (program.verbose) {
+            console.log('>> ' + f);
+            console.log('>>> No docs generated');
           }
         }
       );
